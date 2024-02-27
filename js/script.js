315cf4298924e37886048b93758ffe208227f18b
psychorealist <79111599+psychorealist@users.noreply.github.com>
2024-02-26 22:44:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,24 +15,27 @@ btn.onclick = function() {
   document.querySelector('.terminal').style.color = 'white';
   document.querySelector('.cl').style.animation = 0 ;
   document.querySelector('.terminal').style.color = 'white';
-  document.querySelector('#tixOne').style.opacity = 1 ;
+ //document.querySelector('#tixOne').style.opacity = 1 ;
   document.querySelector('#tixTwo').style.opacity = 1 ;
+  document.querySelector('#locationOne').style.opacity = 1 ;
+  document.querySelector('#locationTwo').style.opacity = 1 ;
+  document.querySelector('#mIOne').style.opacity = 1 ;
+  document.querySelector('#mITwo').style.opacity = 1 ;
 };
+
 
 btn.addEventListener('click', function (){
     txtOne.textContent = `[This week find Giotto at:]`;
-    txtTwo.textContent = `Giotto visual installation 
-    [Party: No Waves EP Launch party]
-     [Location: LE NINETY, 4361 Saint Denis]
-     [Line-up: No waves // Public Appeal // Musique sensible (Mertin Hoak) // Beamski b2b 1800pastamoney //  ]
-    [Tix: $5 online // $10 at the door ]  
-    [Time: Saturday February 23rd (starts 7pm)] `; 
-    txtThree.textContent = `Giotto Live Visuals  
-    [Party: Laylit #65]
-     [Location: Ausgang Plaza,  6524 Rue St-Hubert]
-     [Line-up: Mounsz // Manalou // MNSA // Isa Boom //  ]
-    [Tix: $30 online // $40 at the door ]  
-    [Time: Saturday February 24th, 10pm -3am]`; 
+    txtTwo.textContent = `Giotto Live Visuals and Installation
+    [Party: unknown artist]
+    [Line-up: Vika // Pretty Privilege // Sandinista ]
+    [Time: Wednesday February 28th (10pm - 3am)] 
+    [Tix: Free (limited capacity) ] `; 
+    txtThree.textContent = `Giotto Live Visuals and Installation
+[Party: Nuit Blanche at the Phi Center]
+[Line-up: C Lia // Cheba Iman // Crissmarqueur ]
+[Time: Wednesday March 2nd (9pm - 3am)] 
+ [Tix: Free (limited capacity) ] `; 
     
    
 })
