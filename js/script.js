849518ac27b96e2a7892a9f3e1c955e82ca3e68b
psychorealist <79111599+psychorealist@users.noreply.github.com>
2024-03-14 00:43:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ btn.onclick = function() {
   document.querySelector('.terminal').style.color = 'white';
   document.querySelector('.cl').style.animation = 0 ;
   document.querySelector('.terminal').style.color = 'white';
- //document.querySelector('#tixOne').style.opacity = 1 ;
+  document.querySelector('#tixOne').style.opacity = 1 ;
   document.querySelector('#tixTwo').style.opacity = 1 ;
   document.querySelector('#locationOne').style.opacity = 1 ;
   document.querySelector('#locationTwo').style.opacity = 1 ;
@@ -27,15 +27,11 @@ btn.onclick = function() {
 btn.addEventListener('click', function (){
     txtOne.textContent = `[This week find Giotto at:]`;
     txtTwo.textContent = `Giotto Live Visuals and Installation
-    [Party: unknown artist]
-    [Line-up: Vika // Pretty Privilege // Sandinista ]
-    [Time: Wednesday February 28th (10pm - 3am)] 
-    [Tix: $5 at the door  ] `; 
-    txtThree.textContent = `Giotto Live Visuals and Installation
-[Party: Nuit Blanche at the Phi Center]
-[Line-up: C Lia // Cheba Iman // Crissmarqueur ]
-[Time: Wednesday March 2nd (9pm - 3am)] 
- [Tix: Free (limited capacity) ] `; 
+    [Party: Latex]
+    [Line-up: Crissemarqueur b2b esme // Deepneue // Destiny // Dileta // Divina// Lis Dalton// Syana //]
+    [Time: Wednesday March 15th (10pm - 6am)] 
+    [Tix: $40 ] `; 
+ 
     
    
 })
